4f070a821e59d17cd3788ec0738560cd5cf5776b
kloyi678 <kloyist@gmail.com>
2026-09-25 10:29:32 +0700

    first:commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Mengisi data awal produk supaya aplikasi langsung bisa dicoba
+       
         $this->call(ProdukSeeder::class);
     }
 }
